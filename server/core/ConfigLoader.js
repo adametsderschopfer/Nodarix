222a065daf15fc1828ffd28f82a1338a3db63524
Adamets Vladislav <v.adamets@consult-info.ru>
2021-07-21 23:42:45 +0200

    upd: move env of core

--- server/core/ConfigLoader.js
+++ server/core/ConfigLoader.js
@@ -30,7 +30,7 @@ class ConfigLoader {
     getConfigOfEnv() {
         const filenameEnv = `.${process.env.NODE_ENV}.env`;
 
-        return (this.dotenv.config({ path: this.path.join(this.root, '/server/core/env', filenameEnv) }).parsed);
+        return (this.dotenv.config({ path: this.path.join(this.root, '/env/', filenameEnv) }).parsed);
     }                     
 }
 
